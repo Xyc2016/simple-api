22859d88fe276d3bf6a2047f4f7f73687f2de4eb
xuyucai <xuyucai@meideng.net>
2023-07-23 08:04:04 +0800

    feat(middleware): Support middleware

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
+use async_trait::async_trait;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{header, Body, Method, Request, Response, Server, StatusCode};
-use std::convert::Infallible as Inffallible;
-
-use async_trait::async_trait;
 use once_cell::sync::Lazy;
 use serde_json::Value;
+use std::any::Any;
+use std::borrow::BorrowMut;
 use std::collections::HashMap;
+use std::convert::Infallible as Inffallible;
 use std::net::SocketAddr;
 use std::sync::Arc;
-use std::sync::Mutex;
+use tokio::sync::Mutex;
 
 pub static GLOBAL_SIMPLE_API_INSTANCE: Lazy<Mutex<SimpleApi>> =
     Lazy::new(|| Mutex::new(SimpleApi::new()));
@@ -28,6 +29,16 @@ impl ResT {
     }
 }
 
+impl TryInto<Response<Body>> for ResT {
+    type Error = anyhow::Error;
+    fn try_into(self) -> Result<Response<Body>, Self::Error> {
+        match self {
+            ResT::Json(sc, v) => response_json(v.to_string(), sc),
+            ResT::Raw(v) => anyhow::Ok(v),
+        }
+    }
+}
+
 #[async_trait]
 pub trait ViewHandler: Send + Sync {
     async fn call(&self, req: &mut Request<Body>) -> anyhow::Result<ResT>;
@@ -44,6 +55,42 @@ impl View {
     }
 }
 
+pub struct Context {
+    inner: HashMap<String, Box<dyn Any + Send>>,
+}
+
+impl Context {
+    pub fn new() -> Self {
+        Context {
+            inner: HashMap::new(),
+        }
+    }
+
+    pub fn get<T: 'static + Send>(&self, key: &str) -> Option<&T> {
+        self.inner.get(key).and_then(|v| v.downcast_ref::<T>())
+    }
+
+    pub fn set<T: 'static + Send>(&mut self, key: &str, value: T) {
+        self.inner.insert(key.to_string(), Box::new(value));
+    }
+}
+
+#[async_trait]
+pub trait Middleware: Send + Sync {
+    async fn pre_process(
+        &self,
+        req: &mut Request<Body>,
+        ctx: &mut Context,
+    ) -> anyhow::Result<Option<ResT>>;
+
+    async fn post_process(
+        &self,
+        req: &mut Request<Body>,
+        res: &mut Response<Body>,
+        ctx: &mut Context,
+    ) -> anyhow::Result<Option<ResT>>;
+}
+
 pub fn build_response(
     body_text: String,
     status_code: StatusCode,
@@ -53,7 +100,7 @@ pub fn build_response(
         .status(status_code)
         .body(Body::from(body_text))?;
     r.headers_mut()
-        .insert(header::CONTENT_TYPE, content_type.parse().unwrap());
+        .insert(header::CONTENT_TYPE, content_type.parse()?);
     Ok(r)
 }
 
@@ -61,14 +108,55 @@ pub fn response_json(body_text: String, status_code: StatusCode) -> anyhow::Resu
     build_response(body_text, status_code, "application/json")
 }
 
+pub fn internal_server_error_resp(error: anyhow::Error) -> anyhow::Result<Response<Body>> {
+    build_response(
+        format!("Error: {}", error.to_string()),
+        StatusCode::INTERNAL_SERVER_ERROR,
+        "text/html",
+    )
+}
+
+pub fn result_to_resp(
+    resp_result: anyhow::Result<Response<Body>>,
+) -> anyhow::Result<Response<Body>> {
+    resp_result.or_else(|e| internal_server_error_resp(e))
+}
+
+pub fn result_to_resp_force(resp_result: anyhow::Result<Response<Body>>) -> Response<Body> {
+    return result_to_resp(resp_result).unwrap();
+}
+
+pub async fn apply_middlewares_pre(
+    req: &mut Request<Body>,
+    ctx: &mut Context,
+    middlewares: &Vec<Arc<dyn Middleware>>,
+) -> anyhow::Result<Option<ResT>> {
+    for m in middlewares.iter() {
+        match m.pre_process(req, ctx).await {
+            Ok(None) => continue,
+            other => return other,
+        }
+    }
+    Ok(None)
+}
+
 async fn app_core(mut req: Request<Body>) -> Result<Response<Body>, Inffallible> {
     let path = req.uri().path().to_string();
     let f = GLOBAL_SIMPLE_API_INSTANCE
         .lock()
-        .unwrap()
+        .await
         .routes
         .get_mut(path.as_str())
         .map(|v| v.clone());
+
+    let mut ctx = Context::new();
+    let middlewares = GLOBAL_SIMPLE_API_INSTANCE.lock().await.middlewares.clone();
+
+    match apply_middlewares_pre(&mut req, &mut ctx, &middlewares.lock().await.borrow_mut()).await {
+        Ok(Some(v)) => return Ok(result_to_resp_force(v.try_into())),
+        Ok(None) => (),
+        Err(e) => return Ok(internal_server_error_resp(e).unwrap()),
+    }
 
     match f {
         Some(v) => match v.handler.call(&mut req).await {
@@ -76,12 +164,7 @@ async fn app_core(mut req: Request<Body>) -> Result<Response<Body>, Inffallible>
                 ResT::Json(sc, v) => Ok(response_json(v.to_string(), sc).unwrap()),
                 ResT::Raw(v) => Ok(v),
             },
-            Err(e) => Ok(build_response(
-                format!("Error: {}", e),
-                StatusCode::INTERNAL_SERVER_ERROR,
-                "text/html",
-            )
-            .unwrap()),
+            Err(e) => Ok(internal_server_error_resp(e).unwrap()),
         },
         None => Ok(build_response(
             format!("Not found: {}", path),
@@ -93,18 +176,20 @@ async fn app_core(mut req: Request<Body>) -> Result<Response<Body>, Inffallible>
 }
 
 pub struct SimpleApi {
-    pub routes: HashMap<String, Arc<View>>,
+    routes: HashMap<String, Arc<View>>,
+    middlewares: Arc<Mutex<Vec<Arc<dyn Middleware>>>>,
 }
 
 impl SimpleApi {
     pub fn new() -> Self {
         SimpleApi {
             routes: HashMap::new(),
+            middlewares: Arc::new(Mutex::new(Vec::new())),
         }
     }
 
-    pub fn add_route(path: &str, view: View) {
-        let mut api = GLOBAL_SIMPLE_API_INSTANCE.lock().unwrap();
+    pub async fn add_route(path: &str, view: View) {
+        let mut api = GLOBAL_SIMPLE_API_INSTANCE.lock().await;
         api.routes.insert(path.to_string(), Arc::new(view));
     }
 
@@ -125,5 +210,10 @@ impl SimpleApi {
         if let Err(e) = server.await {
             eprintln!("server error: {}", e);
         }
+    }
+
+    pub async fn add_middleware(m: Arc<dyn Middleware>) {
+        let api = GLOBAL_SIMPLE_API_INSTANCE.lock().await;
+        api.middlewares.lock().await.push(m);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,39 @@
-use hyper::{Body, Method, Request, StatusCode};
+use hyper::{Body, Method, Request, StatusCode, Response};
 use serde_json::json;
+use async_trait::async_trait;
+use simple_api::{ResT, SimpleApi, View, ViewHandler, Middleware, Context};
 
-use simple_api::{ResT, SimpleApi, View, ViewHandler};
+#[derive(Debug)]
+struct Session(String); // dummy session
+
+struct SessionMiddleware;
+#[async_trait]
+impl Middleware for SessionMiddleware {
+    async fn pre_process(
+        &self,
+        req: &mut Request<Body>,
+        ctx: &mut Context,
+    ) -> anyhow::Result<Option<ResT>> {
+        let mut session = Session("dummy".to_string());
+        ctx.set("session", session);
+        Ok(None)
+    }
+
+    async fn post_process(
+        &self,
+        req: &mut Request<Body>,
+        res: &mut Response<Body>,
+        ctx: &mut Context,
+    ) -> anyhow::Result<Option<ResT>> {
+        dbg!(ctx.get::<Session>("session"));
+        Ok(None)
+    }
+
+}
 
 struct Index;
 
-#[async_trait::async_trait]
+#[async_trait]
 impl ViewHandler for Index {
     async fn call(&self, req: &mut Request<Body>) -> anyhow::Result<ResT> {
         ResT::ok_json(json!(
@@ -15,7 +43,7 @@ impl ViewHandler for Index {
 }
 
 struct Unauthed;
-#[async_trait::async_trait]
+#[async_trait]
 impl ViewHandler for Unauthed {
     async fn call(&self, req: &mut Request<Body>) -> anyhow::Result<ResT> {
         ResT::ret_json(
@@ -29,10 +57,11 @@ impl ViewHandler for Unauthed {
 
 #[tokio::main]
 async fn main() {
-    SimpleApi::add_route("/", View::new(vec![Method::GET], Box::new(Index)));
+
+    SimpleApi::add_route("/", View::new(vec![Method::GET], Box::new(Index))).await;
     SimpleApi::add_route(
         "/unauthed",
         View::new(vec![Method::GET], Box::new(Unauthed)),
-    );
+    ).await;
     SimpleApi::run("127.0.0.1:5001").await;
 }
